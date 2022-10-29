7e9cfcc885a1fa1dea2e320fe4896e11df992261
Jenny Verdi <jnyverdi@gmail.com>
2022-10-29 19:59:27 +0700

    Handling PATCH requests

--- app.js
+++ app.js
@@ -25,6 +25,19 @@ app.get('/app/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+app.get('/app/v1/tours/:id', (req, res) => {
+  // chaining parameter => /app/v1/tours/:id/:else/:else
+  // optional parameter => /app/v1/tours/:id/:else?
+  const id = req.params.id * 1;
+  const tour = tours.find((element) => element.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'invalid id' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
 app.post('/app/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
