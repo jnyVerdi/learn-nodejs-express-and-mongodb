102ada27f3aec49ae6c8b216c9feb4beafb282a8
Jenny Verdi <jnyverdi@gmail.com>
2022-10-29 19:58:19 +0700

    Handling POST requests

--- app.js
+++ app.js
@@ -25,6 +25,21 @@ app.get('/app/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+app.post('/app/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
