6bd8d461fc522c19fa3a381e12300ba3daa9dfc3
Jenny Verdi <jnyverdi@gmail.com>
2022-11-05 15:15:42 +0700

    Revert "setting environtment variable"
    This reverts commit 30c3cf8fa9dabd0f2fe64da304cf2a6048c9eac9.

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // * Middleware
-console.log(process.env);
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
