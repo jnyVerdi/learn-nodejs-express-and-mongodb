d906369330d174182c07fe7a4c4ec1c205f11433
Jenny Verdi <jnyverdi@gmail.com>
2022-11-07 15:02:46 +0700

    install mongoose and create initial tour model from mongoose schema

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const mongoose = require('mongoose');
 const app = require('./app');
-const port = 3000;
+
+mongoose
+  .connect(process.env.HOST_DATABASE, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Database connected');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour name is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour price is required'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
